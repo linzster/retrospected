69ee827d16436d5d64c733585078b502bd9b1fd5
Lindsey Harding <lharding@uga.edu>
2013-11-22 23:45:23 -0500

    font changes

--- include/css.php
+++ include/css.php
@@ -1,5 +1,5 @@
 <link href="./assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
 <link href="./assets/css/local.css" rel="stylesheet">
 <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
-<link href='http://fonts.googleapis.com/css?family=Sintony|Ovo' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Noto+Sans|Ovo' rel='stylesheet' type='text/css'>
 
