6427a59e3b3a49ff56657f0d1ea63b93fdb5e242
Brendan Sudol <bsudol@etsy.com>
2013-11-23 02:05:51 -0500

    got sticky footer started

--- index.php
+++ index.php
@@ -1,28 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta http-equiv="content-type" content="text/html; charset=utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
-        <link href="assets/img/favicon.ico" rel="shortcut icon" type="image/x-icon">
-        <title>memory</title>
-        <?php include('include/css.php'); ?>
-        <?php include('include/js.php'); ?>
-    </head>
-	<body>
-        <div class='container'>
-            <h1>Dear Mom and Dad,</h1>
-            <div class='memories'>
-                <h2 class="memory"></h2>
-            </div>
-            <div class="footer">
-                <p>We wanted to share these glimpses of the past, our family's past with you.  We wanted you to know that these 
-                    moments mattered.  That they matter.  When you refresh the page, a new fragment will appear.  We hope to 
-                    keep adding to this, to keep building on to what we have collected so far.  These memories, often triggered 
-                    by a smell or the quality of the air, linger for us.  They persevere, and we am thankful for that.  For in 
-                    them, we are together.</p>
-            </div>
-        </div>
-        <!-- trigger memory randomizer -->
-        <script src="assets/js/memoryRandomizer.js"></script>
-    </body>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+  <link href="assets/img/favicon.ico" rel="shortcut icon" type="image/x-icon">
+  <title>memory</title>
+  <?php include('include/css.php'); ?>
+  <?php include('include/js.php'); ?>
+</head>
+<body>
+
+  <!-- wrapping all page content (to facilitate sticky footer) -->
+  <div id="wrap">
+
+    <div class='container'>
+      <h1>Dear Mom and Dad,</h1>
+      <div class='memories'>
+        <h2 class="memory"></h2>
+      </div>
+    </div>
+
+  </div> <!-- /wrap -->
+
+  <!-- footer -->
+  <div id="footer">
+    <div class='container'>
+      <p>We wanted to share these glimpses of the past, our family's past with you.  We wanted you to know that these 
+      moments mattered.  That they matter.  When you refresh the page, a new fragment will appear.  We hope to 
+      keep adding to this, to keep building on to what we have collected so far.  These memories, often triggered 
+      by a smell or the quality of the air, linger for us.  They persevere, and we am thankful for that.  For in 
+      them, we are together.</p>
+    </div>
+  </div>
+
+  <!-- trigger memory randomizer -->
+  <script src="assets/js/memoryRandomizer.js"></script>
+</body>
 </html>
