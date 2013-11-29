9fa7877841f0f68f7afd22dc7b83681a9458f4cd
Brendan Sudol <bsudol@etsy.com>
2013-11-29 10:41:31 -0500

    allow for new memory without full page refresh

--- assets/js/memoryRandomizer.js
+++ assets/js/memoryRandomizer.js
@@ -1,39 +1,49 @@
 $(document).ready(function() {
 
-  // random color for background 
-  var colors = ["#914ad3","#d34a8c","#0080FF","#4ad391", "#34495e", "#fb887c"];
-  var rand_color = Math.floor(Math.random() * colors.length);
+  function getRandomMemory() {
 
-  // storing memories in an array
-  var memories = [];
+    // random color for background 
+    var colors = ["#914ad3","#d34a8c","#0080FF","#4ad391", "#34495e", "#fb887c"];
+    var rand_color = Math.floor(Math.random() * colors.length);
 
-  memories.push("Bike ride at Maraine State Park.  Lunch on a pier.  Crunching Doritos beneath a marshmallow cloud sky.");
-  memories.push("That sandwhich place outside Hamilton College.  Best sandwhich I have ever had.  Still.");
-  memories.push("Walks, walking, through cemeteries, to coffeeshops, and later pushing strollers.");
-  memories.push("Do you remember?  I do.  These moments I carry with me.  Is it like that for you?");
-  memories.push("I've thought about texting you, these shimmering glimpses of the past. But sometimes I don't have my phone, and even if I did, the medium didn't seem right.");
-  memories.push("Texts don't hold enough weight, and the lightness would betray something, I fear.  Something I want you to know.");
-  memories.push("They surface when life settles for an instant.  Then I'm back, back in time, back with you, and these memories are moments, are happening again.");  
-  memories.push("Mom, in your purple robe, stepping out into the Chautauqua morning.  I could feel the sun, early as it was.");
-  memories.push("Being rocked through ear infections.  The pain pacified by back and forth, back and forth.");
-  memories.push("Windows down, Bob Dylan loud.  'Everybody must get stoned.'  Driving in circles in Timberglen.  Did that song ever end?");
-  memories.push("Windows down, the morning dark on our way to school.  Windows up, and warmer.  At least that was the idea, right");
-  memories.push("These moments aren't pinnable.  We don't blog about them.  They are traces.  They return.  They last.");
-  // keep adding items here...
+    // storing memories in an array
+    var memories = [];
 
-  // compute total number of memories and pick random number from within
-  var total = memories.length;
-  var rand_mem = Math.floor(Math.random() * total);
+    memories.push("Bike ride at Maraine State Park.  Lunch on a pier.  Crunching Doritos beneath a marshmallow cloud sky.");
+    memories.push("That sandwhich place outside Hamilton College.  Best sandwhich I have ever had.  Still.");
+    memories.push("Walks, walking, through cemeteries, to coffeeshops, and later pushing strollers.");
+    memories.push("Do you remember?  I do.  These moments I carry with me.  Is it like that for you?");
+    memories.push("I've thought about texting you, these shimmering glimpses of the past. But sometimes I don't have my phone, and even if I did, the medium didn't seem right.");
+    memories.push("Texts don't hold enough weight, and the lightness would betray something, I fear.  Something I want you to know.");
+    memories.push("They surface when life settles for an instant.  Then I'm back, back in time, back with you, and these memories are moments, are happening again.");  
+    memories.push("Mom, in your purple robe, stepping out into the Chautauqua morning.  I could feel the sun, early as it was.");
+    memories.push("Being rocked through ear infections.  The pain pacified by back and forth, back and forth.");
+    memories.push("Windows down, Bob Dylan loud.  'Everybody must get stoned.'  Driving in circles in Timberglen.  Did that song ever end?");
+    memories.push("Windows down, the morning dark on our way to school.  Windows up, and warmer.  At least that was the idea, right");
+    memories.push("These moments aren't pinnable.  We don't blog about them.  They are traces.  They return.  They last.");
+    // keep adding items here...
 
-  // add random memory and background color to page
-  $(".memory").text(memories[rand_mem]);
-  $('body').css("background", colors[rand_color]);
+    // compute total number of memories and pick random number from within
+    var total = memories.length;
+    var rand_mem = Math.floor(Math.random() * total);
 
-  // // localstore stuff (TODO: UPDATE!)
-  // if (!localStorage.pageCounter) {
-  //   localStorage.setItem('pageCounter',0);
-  // }
-  // localStorage.setItem('pageCounter',parseInt(localStorage.pageCounter)+1);
-  // console.log(localStorage.pageCounter);
+    // add random memory and background color to page
+    $(".memory").text(memories[rand_mem]);
+    $('body').css("background", colors[rand_color]);
+
+    // // localstore stuff (TODO: UPDATE!)
+    // if (!localStorage.pageCounter) {
+    //   localStorage.setItem('pageCounter',0);
+    // }
+    // localStorage.setItem('pageCounter',parseInt(localStorage.pageCounter)+1);
+    // console.log(localStorage.pageCounter);
+
+  }
+
+  // initial memory on page load
+  getRandomMemory();
+
+  // generate and display new memory on click
+  $('.refresh').click(function() { getRandomMemory(); });
 
 });
--- index.php
+++ index.php
@@ -4,17 +4,17 @@
   <meta http-equiv="content-type" content="text/html; charset=utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
   <link href="assets/img/favicon.ico" rel="shortcut icon" type="image/x-icon">
-  <title>memory</title>
+  <title>Retrospected</title>
   <?php include('include/css.php'); ?>
 </head>
 <body>
 
-  <!-- wrapping all page content (to facilitate sticky footer) -->
+  <!-- wrapping all page content (except sticky footer) -->
   <div id="wrap">
 
     <!-- navbar -->
     <div class="navbar navbar-default">
-      <a class="navbar-brand" href="javascript:history.go(0)"><i class="icon-pencil"></i> Retrospected </a>
+      <a class="navbar-brand refresh" href="#"><i class="icon-pencil"></i> Retrospected </a>
       <p class="navbar-text author-line hidden-xs">Designed and built with <i class="icon-heart-empty"></i> by Linz & Bren</p>
       <a class="navbar-text pull-right open-slider" href="#slider-info">Learn more →</a>
     </div>
@@ -26,30 +26,29 @@
       </div>
     </div>
 
+    <!-- modal -->
+    <div id="slider-info">
+      <h3>We love you, and...</h3>
+      <p>
+        we wanted to share these glimpses of the past, our family's past with you.  We wanted you to know that these 
+        moments mattered.  That they matter.  When you refresh the page, a new fragment will appear.  We hope to 
+        keep adding to this, to keep building on to what we have collected so far.  These memories, often triggered 
+        by a smell or the quality of the air, linger for us.  They persevere, and we are thankful for that.  For in 
+        them, we are together.
+      </p>
+      <button type="button" class="btn btn-default close-slider">Close</button>
+    </div>
+
   </div> <!-- /wrap -->
 
   <!-- footer -->
   <div id="footer">
     <div class="container text-center">
-      <a class='refresh' href="javascript:history.go(0)">And this...</a>
+      <a class='refresh' href="#">And this...</a>
     </div>
   </div>
 
-  <!-- modal -->
-  <div id="slider-info">
-    <h3>We love you, and...</h3>
-    <p>
-      we wanted to share these glimpses of the past, our family's past with you.  We wanted you to know that these 
-      moments mattered.  That they matter.  When you refresh the page, a new fragment will appear.  We hope to 
-      keep adding to this, to keep building on to what we have collected so far.  These memories, often triggered 
-      by a smell or the quality of the air, linger for us.  They persevere, and we are thankful for that.  For in 
-      them, we are together.
-    </p>
-    <button type="button" class="btn btn-default close-slider">Close</button>
-  </div>
-  
   <!-- js placed at end of document so page loads faster -->
   <?php include('include/js.php'); ?>
-
 </body>
 </html>
