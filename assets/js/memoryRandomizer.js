84d5a40ef2caa9c662284e48ebe439501cfa67be
Lindsey Harding <lharding@uga.edu>
2013-12-17 22:36:32 -0500

    more memories and modal changes

--- assets/js/memoryRandomizer.js
+++ assets/js/memoryRandomizer.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     // storing memories in an array
     var memories = [];
 
-    memories.push("Bike ride at Maraine State Park.  Lunch on a pier.  Crunching Doritos beneath a marshmallow cloud sky.");
+    memories.push("Bike ride at Moraine State Park.  Lunch on a pier.  Crunching Doritos beneath a marshmallow cloud sky.");
     memories.push("That sandwhich place outside Hamilton College.  Best sandwhich I have ever had.  Still.");
     memories.push("Walks, walking, through cemeteries, to coffeeshops, and later pushing strollers.");
     memories.push("Do you remember?  I do.  These moments I carry with me.  Is it like that for you?");
@@ -19,9 +19,15 @@ $(document).ready(function() {
     memories.push("Mom, in your purple robe, stepping out into the Chautauqua morning.  I could feel the sun, early as it was.");
     memories.push("Being rocked through ear infections.  The pain pacified by back and forth, back and forth.");
     memories.push("Windows down, Bob Dylan loud.  'Everybody must get stoned.'  Driving in circles in Timberglen.  Did that song ever end?");
-    memories.push("Windows down, the morning dark on our way to school.  Windows up, and warmer.  At least that was the idea, right");
+    memories.push("Windows down, the morning dark on our way to school.  Windows up, and warmer.  At least that was the idea, right?");
     memories.push("These moments aren't pinnable.  We don't blog about them.  They are traces.  They return.  They last.");
-    // keep adding items here...
+    memories.push("The news had gone out of the first world war...Christmas bells, those Christmas bells...");
+    memories.push("Weekend mornings riding downtown to basketball at Reizenstein.  There, game after game, basketballs against floorboards, against backboards endlessly.  And you in the stands.");
+    memories.push("Turn-signal on, lights off, police car turning towards and I almost side-swipe a car.  You yelling: 'Stop the car.  Just stop the car!'  So I did.  In the middle of the road.  But I drove the rest of the way home, knuckles white.  Cautious.  Terrified.");
+    memories.push("Graduation party and a human fly wall in our driveway.  All night, we jumped against the wall, got stuck, and wrestled free.  We do that now: jump, stick, wrestle free.  Sometimes we hurdle the wall, and sometimes we need help getting down.");
+    memories.push("It doesn't get any better than this.");
+
+// keep adding items here...
 
     // compute total number of memories and pick random number from within
     var total = memories.length;
